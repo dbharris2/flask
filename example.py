4c1602f30804ffa2cb00ac87fc5bb66efdfd7b39
Devon Harris <dbharris2@gmail.com>
2017-10-01 15:22:52 -0700

    Fetch and display a random Chuck Norris joke

--- example.py
+++ example.py
@@ -2,11 +2,19 @@ from flask import Flask
 from mongo_json_encoder import MongoJSONEncoder
 from pymongo import MongoClient
 import os
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def joke():
+    response = requests.get('http://api.icndb.com/jokes/random')
+    json = response.json()
+    joke = json['value']['joke']
+    return joke
+
+@app.route("/fitbit")
+def fitbit():
     mongo_client = MongoClient(os.environ['MONGODB_URI'])
     db = mongo_client.fitbit_users
     results = {}
