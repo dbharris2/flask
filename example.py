991a0cdc0d0a27bd19826af15dfa8d6aa9fafae6
Devon Harris <dbharris2@gmail.com>
2017-10-01 14:56:44 -0700

    Move json encoder to its own class

--- example.py
+++ example.py
@@ -1,17 +1,9 @@
-from bson import ObjectId
-from flask import Flask, jsonify
+from flask import Flask
+from mongo_json_encoder import MongoJSONEncoder
 from pymongo import MongoClient
-import json
 import os
-import urllib
 
 app = Flask(__name__)
-
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
 
 @app.route("/")
 def hello():
@@ -20,7 +12,7 @@ def hello():
     results = {}
     for user in db.users.find():
       results[user['user_id']] = user
-    return JSONEncoder().encode(results)
+    return MongoJSONEncoder().encode(results)
 
 if __name__ == "__main__":
     app.run()
